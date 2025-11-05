775f1d6c6a168a06d331316dd15641a5b3b90bde
sadhon kumar dey <sadhonkumardey750@gmail.com>
2025-11-06 01:48:48 +0600

    Campus-mart

--- src/Componants/BookList.jsx
+++ src/Componants/BookList.jsx
@@ -1,3 +1,6 @@
+import { MdAddShoppingCart } from "react-icons/md";
+import { IoBagAdd } from "react-icons/io5";
+
 export default function BookList({ books }) {
   return (
     <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
@@ -15,9 +18,9 @@ export default function BookList({ books }) {
           <p className="text-gray-600 text-sm">Code: {book.code}</p>
           <p className="text-gray-700 font-medium mt-1">Price : {book.price}</p>
           <p className="font-semibold text-blue-800">{book.publication}</p>
-          <div className="pt-3">
+          <div className="pt-3 flex justify-between items-center">
             <button
-              className="btn w-full h-[35px] pt-  bg-linear-to-r from-[#2193b0] to-[#6dd5ed] text-white border-none 
+              className="btn w- h-[35px] pt-  bg-linear-to-r from-[#2193b0] to-[#6dd5ed] text-white border-none 
          text-[15px] font-bold cursor-pointer relative z-1 overflow-hidden
          hover:text-black
          before:content-[''] before:absolute before:top-0 before:bottom-0
@@ -26,7 +29,20 @@ export default function BookList({ books }) {
          before:transition-all before:duration-500
          hover:before:transform-[skewX(-45deg)_scaleX(1)_scaleY(1)] rounded-2xl"
             >
+              <p className="text-xl font-bold">
+                <MdAddShoppingCart />
+              </p>{" "}
               {book.button}
+            </button>
+            <button
+              className=" flex items-center gap-2  bg-amber-500 text-white
+                hover:bg-gray-300 h-[35px] px-5 rounded-2xl font-bold duration-300 hover:text-black"
+            >
+              <p className="font-bold text-xl ">
+                {" "}
+                <IoBagAdd></IoBagAdd>{" "}
+              </p>{" "}
+              Buy Now{" "}
             </button>
           </div>
         </div>
